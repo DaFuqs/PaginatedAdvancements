94cea1d634cdc8a0d955525d6ace877bcb17c3a8
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-06-12 19:07:32 +0200

    started 1.20 port. More todo

--- src/main/java/de/dafuqs/paginatedadvancements/client/PaginatedAdvancementTabType.java
+++ src/main/java/de/dafuqs/paginatedadvancements/client/PaginatedAdvancementTabType.java
@@ -2,37 +2,35 @@ package de.dafuqs.paginatedadvancements.client;
 
 import de.dafuqs.paginatedadvancements.*;
 import net.minecraft.client.gui.*;
-import net.minecraft.client.render.item.*;
-import net.minecraft.client.util.math.*;
+import net.minecraft.client.gui.screen.advancement.*;
 import net.minecraft.item.*;
-import org.jetbrains.annotations.*;
 
-public class PaginatedAdvancementTabType extends DrawableHelper {
-    
-    public static final int WIDTH = 28;
-    public static final int HEIGHT = 32;
-    
-    public static int getWidthWithSpacing() {
-        return WIDTH + PaginatedAdvancementsClient.CONFIG.SpacingBetweenHorizontalTabs; // includes the empty space between tabs
-    }
-    
-    public static void drawBackground(MatrixStack matrices, DrawableHelper tab, int x, int y, boolean selected, int index) {
-        int i = index > 0 ? WIDTH : 0;
-        int j = selected ? HEIGHT : 0;
-        tab.drawTexture(matrices, x + getTabX(index), y + getTabY(), i, j, WIDTH, HEIGHT);
-    }
-
-    public static void drawIcon(int x, int y, int index, @NotNull ItemRenderer itemRenderer, ItemStack icon) {
-        int i = x + getTabX(index) + 6;
-        int j = y + getTabY() + 9;
-        itemRenderer.renderInGui(icon, i, j);
-    }
-
-    public static int getTabX(int index) {
-        return getWidthWithSpacing() * index;
-    }
-
-    public static int getTabY() {
+public class PaginatedAdvancementTabType {
+	
+	public static final int WIDTH = 28;
+	public static final int HEIGHT = 32;
+	
+	public static int getWidthWithSpacing() {
+		return WIDTH + PaginatedAdvancementsClient.CONFIG.SpacingBetweenHorizontalTabs; // includes the empty space between tabs
+	}
+	
+	public static void drawBackground(DrawContext context, int x, int y, boolean selected, int index) {
+		int i = index > 0 ? WIDTH : 0;
+		int j = selected ? HEIGHT : 0;
+		context.drawTexture(AdvancementsScreen.TABS_TEXTURE, x + getTabX(index), y + getTabY(), i, j, WIDTH, HEIGHT);
+	}
+	
+	public static void drawIcon(DrawContext context, int x, int y, int index, ItemStack stack) {
+		int i = x + getTabX(index) + 6;
+		int j = y + getTabY() + 9;
+		context.drawItemWithoutEntity(stack, i, j);
+	}
+	
+	public static int getTabX(int index) {
+		return getWidthWithSpacing() * index;
+	}
+	
+	public static int getTabY() {
         return -HEIGHT + 4;
     }
 
--- src/main/java/de/dafuqs/paginatedadvancements/client/PinnedAdvancementTabType.java
+++ src/main/java/de/dafuqs/paginatedadvancements/client/PinnedAdvancementTabType.java
@@ -2,38 +2,36 @@ package de.dafuqs.paginatedadvancements.client;
 
 import de.dafuqs.paginatedadvancements.*;
 import net.minecraft.client.gui.*;
-import net.minecraft.client.render.item.*;
-import net.minecraft.client.util.math.*;
+import net.minecraft.client.gui.screen.advancement.*;
 import net.minecraft.item.*;
-import org.jetbrains.annotations.*;
 
-public class PinnedAdvancementTabType extends DrawableHelper {
-    
-    public static final int TOP_SPACING = 24; // accounting for the "pin" ribbon
-    public static final int WIDTH = 32;
-    public static final int HEIGHT = 28;
-    
-    public static int getHeightWithSpacing() {
-        return HEIGHT + PaginatedAdvancementsClient.CONFIG.SpacingBetweenPinnedTabs; // includes the empty space between tabs
-    }
-    
-    public static void drawBackground(MatrixStack matrices, DrawableHelper tab, int x, int y, boolean selected, int index) {
-        int i = index > 0 ? WIDTH + 96 : 96;
-        int j = selected ? 64 + HEIGHT : 64;
-        tab.drawTexture(matrices, x + getTabX(), y + getTabY(index), i, j, WIDTH, HEIGHT);
-    }
-
-    public static void drawIcon(int x, int y, int index, @NotNull ItemRenderer itemRenderer, ItemStack icon) {
-        int i = x + getTabX() + 6;
-        int j = y + getTabY(index) + 5;
-        itemRenderer.renderInGui(icon, i, j);
-    }
-
-    public static int getTabX() {
-        return WIDTH - PaginatedAdvancementScreen.BORDER_PADDING - 4;
-    }
-
-    public static int getTabY(int index) {
+public class PinnedAdvancementTabType {
+	
+	public static final int TOP_SPACING = 24; // accounting for the "pin" ribbon
+	public static final int WIDTH = 32;
+	public static final int HEIGHT = 28;
+	
+	public static int getHeightWithSpacing() {
+		return HEIGHT + PaginatedAdvancementsClient.CONFIG.SpacingBetweenPinnedTabs; // includes the empty space between tabs
+	}
+	
+	public static void drawBackground(DrawContext context, int x, int y, boolean selected, int index) {
+		int i = index > 0 ? WIDTH + 96 : 96;
+		int j = selected ? 64 + HEIGHT : 64;
+		context.drawTexture(AdvancementsScreen.TABS_TEXTURE, x + getTabX(), y + getTabY(index), i, j, WIDTH, HEIGHT);
+	}
+	
+	public static void drawIcon(DrawContext context, int x, int y, int index, ItemStack stack) {
+		int i = x + getTabX() + 6;
+		int j = y + getTabY(index) + 5;
+		context.drawItemWithoutEntity(stack, i, j);
+	}
+	
+	public static int getTabX() {
+		return WIDTH - PaginatedAdvancementScreen.BORDER_PADDING - 4;
+	}
+	
+	public static int getTabY(int index) {
         return TOP_SPACING + getHeightWithSpacing() * index;
     }
 
