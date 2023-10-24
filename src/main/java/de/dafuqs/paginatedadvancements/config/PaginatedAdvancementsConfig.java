0a13c6b104e2c02d48ad28f1ba43797d84ec9c2c
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-10-24 10:26:36 +0200

    removed duplicate setting tooltips. Fixes #15

--- src/main/java/de/dafuqs/paginatedadvancements/config/PaginatedAdvancementsConfig.java
+++ src/main/java/de/dafuqs/paginatedadvancements/config/PaginatedAdvancementsConfig.java
@@ -2,7 +2,6 @@ package de.dafuqs.paginatedadvancements.config;
 
 import me.shedaniel.autoconfig.*;
 import me.shedaniel.autoconfig.annotation.*;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.*;
 import net.minecraft.client.*;
 
 import java.util.*;
@@ -10,34 +9,16 @@ import java.util.*;
 @Config(name = "PaginatedAdvancements")
 public class PaginatedAdvancementsConfig implements ConfigData {
 	
-	@Comment("Enable pinning of advancement tabs")
 	public boolean PinningEnabled = true;
-	
-	@Comment("Show advancement ID in debug tooltip")
 	public boolean ShowAdvancementIDInDebugTooltip = true;
-	
-	@Comment("Show Debug Info on advancement hover (possible values: 'always', 'never', 'debug_tooltips_enabled')")
 	public DebugInfoSetting ShowDebugInfo = DebugInfoSetting.DEBUG_TOOLTIPS_ENABLED;
-	
-	@Comment("Max displayed advancement criterion entries when not pressing shift")
 	public int MaxCriterionEntries = 2;
-	
-	@Comment("Fade the background slightly gray when hovering over an advancement")
 	public boolean FadeOutBackgroundOnAdvancementHover = true;
-	
-	@Comment("Save and restore the last selected tab")
 	public boolean SaveLastSelectedTab = true;
-	
-	@Comment("List of pinned Tabs (root advancement identifier)")
 	public List<String> PinnedTabs = new ArrayList<>();
-	
-	@Comment("The identifier of the last opened tab")
 	public String LastSelectedTab = "";
-	
-	@Comment("Spacing between horizontal tabs (vanilla default: 4)")
+	@ConfigEntry.Gui.Tooltip
 	public int SpacingBetweenHorizontalTabs = 4;
-	
-	@Comment("Spacing between pinned tabs")
 	public int SpacingBetweenPinnedTabs = 2;
 	
 	public enum DebugInfoSetting {
